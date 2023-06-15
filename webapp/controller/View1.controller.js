684dcd0b593c6a0180e6dc865fa66ebd968dde6a
PatelKurvesh <patelkurvesh1999@gmail.com>
2023-06-15 15:19:49 +0530

    add some validation

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,10 +1,16 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
-	"sap/ui/model/json/JSONModel"
-], function(Controller,JSONModel) {
+	"sap/ui/model/json/JSONModel",
+	"sap/ui/export/library",
+	"sap/ui/export/Spreadsheet"
+], function(Controller,JSONModel,exportLibrary,Spreadsheet) {
 	"use strict";
-
+	
+	var EdmType = exportLibrary.EdmType;
+	
 	return Controller.extend("vsdSAP-CreateRows.controller.View1", {
+		
+	
 		
 		onInit : function(){
 			var oJson = new JSONModel({
@@ -16,7 +22,7 @@ sap.ui.define([
 		onCreate : function(){
 			var oModel = this.getView().getModel("Json");
 			var oInput = oModel.getProperty("/InputValue");
-			if (oInput){
+			if (oInput>0){
 				for (var i = 1 ; i <= oInput ; i++ ){
 					var oData = oModel.getProperty("/Data");
 					oData.push({
@@ -26,14 +32,20 @@ sap.ui.define([
 					oModel.setProperty("/Data" , oData);
 					oModel.refresh(true);
 				}
+			}else{
+				this.getView().byId("lbl").setText("Please Enter Valid Positive Number");
 			}
 		},
 		
 		onSave : function(){
+			sap.m.MessageToast.show("Data is Store Successfully");
 			var oModel = this.getView().getModel("Json");
 			var oNewData = oModel.getProperty("/Data");
 			oModel.setProperty("/newData" , oNewData);
-		}
+		},
+		
+	
+
 
 	});
 });
