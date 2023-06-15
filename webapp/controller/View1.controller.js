f4dc1755bd7b99dde9e85091166f45730bcfe72a
PatelKurvesh <patelkurvesh1999@gmail.com>
2023-06-15 14:35:40 +0530

    Completed

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,9 +1,39 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function(Controller) {
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/model/json/JSONModel"
+], function(Controller,JSONModel) {
 	"use strict";
 
 	return Controller.extend("vsdSAP-CreateRows.controller.View1", {
+		
+		onInit : function(){
+			var oJson = new JSONModel({
+				Data : []
+			});
+			this.getView().setModel(oJson , "Json");
+		},
+		
+		onCreate : function(){
+			var oModel = this.getView().getModel("Json");
+			var oInput = oModel.getProperty("/InputValue");
+			if (oInput){
+				for (var i = 1 ; i <= oInput ; i++ ){
+					var oData = oModel.getProperty("/Data");
+					oData.push({
+						"No" : null,
+						"Name" : null
+					});
+					oModel.setProperty("/Data" , oData);
+					oModel.refresh(true);
+				}
+			}
+		},
+		
+		onSave : function(){
+			var oModel = this.getView().getModel("Json");
+			var oNewData = oModel.getProperty("/Data");
+			oModel.setProperty("/newData" , oNewData);
+		}
 
 	});
 });
